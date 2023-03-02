bfc46bdcfcb8170e700e98db51eafda254ce1540
Fernando Castilho <castilho.wylde@gmail.com>
2023-03-02 20:30:11 -0300

    feat: added client repository on app layer

---
+++ src/app/repositories/client/client.repository.ts
@@ -0,0 +1,22 @@
+import BaseRepository from '../base/base.repository';
+import ClientEntity from '../../../domain/entities/client/client.entity';
+
+abstract class ClientRepository extends BaseRepository<ClientEntity> {
+  abstract findAll(): Promise<ClientEntity[]>
+
+  abstract findMany(filter: Partial<ClientEntity>): Promise<ClientEntity[]>
+
+  abstract findById(id: string): Promise<ClientEntity>
+
+  abstract findOne(filter: Partial<ClientEntity>): Promise<ClientEntity>
+
+  abstract save(entity: ClientEntity): Promise<ClientEntity>
+
+  abstract update(id: string, entity: ClientEntity): Promise<ClientEntity>
+
+  abstract patch(id: string, partialEntity: Partial<ClientEntity>): Promise<ClientEntity>
+
+  abstract delete(id: string): Promise<void>
+}
+
+export default ClientRepository;
