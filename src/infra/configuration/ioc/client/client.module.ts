6ece058b2db0bbf99bb1ffb251a6a85e1cbe0dc2
Fernando Castilho <castilho.wylde@gmail.com>
2023-04-07 01:28:04 -0300

    feat: change client module to global module

--- src/infra/configuration/ioc/client/client.module.ts
+++ src/infra/configuration/ioc/client/client.module.ts
@@ -9,6 +9,7 @@ import FindAllClientsUseCase from '../../../../domain/usecases/client/find-all-c
 import FindManyClientsUseCase from '../../../../domain/usecases/client/find-many-clients.use-case';
 import FindOneClientByClientUseCase from '../../../../domain/usecases/client/find-one-client-by-client.use-case';
 import FindOneClientByUidUseCase from '../../../../domain/usecases/client/find-one-client-by-uid.use-case';
+import FindOneClientByApiKeyUseCase from '../../../../domain/usecases/client/find-one-client-by-api-key.use-case';
 import FindOneClientByHostUseCase from '../../../../domain/usecases/client/find-one-client-by-host.use-case';
 import PatchClientUseCase from '../../../../domain/usecases/client/patch-client.use-case';
 import { clientSchemaOptions } from '../database/schemas/client.schema';
@@ -25,6 +26,7 @@ import { clientSchemaOptions } from '../database/schemas/client.schema';
     FindOneClientByClientUseCase,
     FindOneClientByUidUseCase,
     FindOneClientByHostUseCase,
+    FindOneClientByApiKeyUseCase,
     PatchClientUseCase,
     ClientService,
   ],
